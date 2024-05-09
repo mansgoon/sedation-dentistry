5c400757c8efc94deb8f7fb7f5b4441978d621c4
mansgoon <scrubpvp911@gmail.com>
2024-05-09 13:36:07 -0400

    added react time picker to contact form

--- components/contact.js
+++ components/contact.js
@@ -2,6 +2,8 @@
 import * as React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
+import 'react-time-picker/dist/TimePicker.css';
 import Footer from "./footer.js";
 
 function ContactSection() {
@@ -18,13 +20,20 @@ function ContactSection() {
 }
 
 function ContactForm() {
+  
     const [selectedDate, setSelectedDate] = React.useState(null);
+    const [selectedTime, setSelectedTime] = React.useState('10:00');
+
+    const handleTimeChange = (event) => {
+      setSelectedTime(event.target.value);
+    };
+
     return (
       <div className="box-border flex relative flex-col shrink-0 pb-8 mr-52 mt-10 h-auto rounded-lg border-2 border-solid border-[#5BA3BB] max-md:mx-5 max-sm:mx-5">
         <div className="flex gap-5 max-sm:flex-col max-md:gap-0">
           <div className="flex flex-col w-1/2 max-sm:ml-0 max-sm:w-full">
             <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5 mt-5 h-auto">
-              <label htmlFor="firstName" className="box-border relative shrink-0 mt-5 h-auto text-zinc-800 font-medium">
+              <label htmlFor="firstName" className="box-border relative shrink-0 mt-5 text-zinc-800 font-medium">
                 First name
               </label>
               <input
@@ -37,7 +46,7 @@ function ContactForm() {
               />
             </div>
           </div>
-          <div className="flex flex-col ml-5 w-1/2 max-sm:ml-0 max-sm:w-full">
+          <div className="flex flex-col  w-1/2 max-sm:ml-0 max-sm:w-full">
           <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5 mt-5 h-auto">
             <label htmlFor="lastName" className="box-border relative shrink-0 mt-5 h-auto text-zinc-800 font-medium">
               Last name
@@ -79,18 +88,37 @@ function ContactForm() {
             required
           />
         </div>
-        <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5 h-auto">
-            <label htmlFor="date" className="box-border relative shrink-0 mt-5 h-auto text-zinc-800 font-medium">
-            Select date
-            </label>
-            <DatePicker
-            id="date"
-            name="date"
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-            className="box-border flex relative flex-col shrink-0 w-full mt-5 rounded border border-solid border-zinc-400 text-[#282828] focus:outline-none focus:ring-1 caret-zinc-800 p-2.5"
-            placeholderText="Select a date"
-            />
+        <div className="flex gap-5 max-sm:flex-col max-md:gap-0">
+          <div className="flex flex-col w-1/2 max-sm:ml-0 max-sm:w-full">
+            <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5 h-auto">
+              <label htmlFor="date" className="box-border relative shrink-0 mt-5 h-auto text-zinc-800 font-medium">
+                Select date
+              </label>
+              <DatePicker
+                id="date"
+                name="date"
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                className="box-border flex relative flex-col shrink-0 w-full mt-5 rounded border border-solid border-zinc-400 text-[#282828] focus:outline-none focus:ring-1 caret-zinc-800 p-2.5"
+                placeholderText="Select a date"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-1/2 max-sm:ml-0 max-sm:w-full">
+            <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5  h-auto">
+              <label htmlFor="time" className="box-border relative shrink-0 mt-5 h-auto text-zinc-800 font-medium"> 
+                Select time
+              </label>
+              <input
+                type="time"
+                id="time"
+                name="time"
+                value={selectedTime}
+                onChange={handleTimeChange}
+                className="box-border flex relative flex-col shrink-0 w-full p-2.5 mt-5 rounded border border-solid border-zinc-400 caret-zinc-800 text-[#282828] focus:outline-none focus:ring-1"
+              />
+            </div>
+          </div>
         </div>
         <div className="box-border flex relative flex-col shrink-0 pb-8 mx-5 h-auto">
           <label htmlFor="messageBox" className="box-border relative shrink-0 mt-5 h-auto font-medium text-zinc-800">
